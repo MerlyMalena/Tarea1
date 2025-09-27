d5731cba0a3ddcd46b0ac6171229dcbc608d7de7
MerlyMalena <merlymalena2303@hotmail.com>
2025-09-27 18:05:00 -0400

    Add files via upload

---
+++ PareImpar/src/com/PareImpar.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com;
+
+/**
+ *
+ * @author Merly
+ */
+public class PareImpar {
+    public static void main(String[] args) {
+        // 8. Realizar un programa que ingresado un numero, me indique si es par o impar.
+        int num=30062025;
+        
+        if(num%2==0)
+        System.out.println(num+" es par.");
+        else {
+            System.out.println(num+ " es impar.");
+        }
+    }
+}
