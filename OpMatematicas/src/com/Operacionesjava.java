996da1f2dce5db0d3e15028d5f86bb328f853ef2
MerlyMalena <merlymalena2303@hotmail.com>
2025-09-27 18:00:03 -0400

    Add files via upload

---
+++ OpMatematicas/src/com/Operacionesjava.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com;
+
+/**
+ *
+ * @author Merly
+ */
+public class Operacionesjava {
+
+    public static void main(String[] args) {
+        // 2. Realizar un programa que incluya las 4 operaciones matemáticas básicas (Suma, Resta, Multiplicación y División).
+        int valor1= 10;
+        int valor2=5;
+        
+        System.out.println(valor1 +"+" +valor2 +"=" +(valor1+valor2));
+        System.out.println(valor1 +"-" +valor2 +"=" +(valor1-valor2));
+        System.out.println(valor1 +"*" +valor2 +"=" +(valor1*valor2));
+        System.out.println(valor1 +"/" +valor2 +"=" +(valor1/valor2));
+    }
+    
+
+
+}
