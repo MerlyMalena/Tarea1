c7c3699451e21c5193f2922b8432bc604f7d506d
MerlyMalena <merlymalena2303@hotmail.com>
2025-09-27 18:04:21 -0400

    Add files via upload

---
+++ MayorMenor/src/com/MayorMenor.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com;
+
+/**
+ *
+ * @author Merly
+ */
+public class MayorMenor {
+    public static void main(String[] args) {
+        // 7. Realizar un programa que dado dos números, me indique cual es el mayor y cual es el menor de ambos.
+        int n1= 23;
+    int n2= 3;
+    
+    if (n1>n2){
+    System.out.println(n1+" es mayor que "+ n2);
+    }
+    else {
+        System.out.println(n2+" es mayor que "+n1);
+    }
+    
+}
+    
+}
