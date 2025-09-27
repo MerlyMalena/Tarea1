92bc7d0c2b7a26691f6336fb6fb2272b6d8aaa4b
MerlyMalena <merlymalena2303@hotmail.com>
2025-09-27 17:59:00 -0400

    Add files via upload

---
+++ DatosPersonales/src/com/DatosPersonales.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com;
+
+/**
+ *
+ * @author Merly
+ */
+public class DatosPersonales {
+    public static void main(String[] args) {
+        // 1. Realizar un programa que imprima por pantalla su nombre y correo electrónico.
+        String nombre="Merly Malena Hernández";
+        String correo="20251137@itla.edu.do";
+        System.out.println("Nombre:"+nombre);
+        System.out.println("Correo Electrónico:"+correo);
+    }
+}
