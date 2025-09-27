19ec081f5fb4590e74cdce1cd2e5dd8227514b41
MerlyMalena <merlymalena2303@hotmail.com>
2025-09-27 18:02:22 -0400

    Add files via upload

---
+++ NumerosParesdel2al100/src/com/NumParesdel2al100.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com;
+
+/**
+ *
+ * @author Merly
+ */
+public class NumParesdel2al100 {
+    public static void main(String[] args) {
+     // 4. Realizar un programa que imprima los números pares del 2 al 100 utilizando ciclos "while".
+int i=1;
+while (i<=100){
+    if (i%2==0){
+        System.out.println(i);
+    }
+    i++;
+} 
+
+    }
+}
